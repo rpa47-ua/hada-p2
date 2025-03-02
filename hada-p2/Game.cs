3bf43e0a79ee71af2b52ec4f56d4bfbfeca9b32c
Francisco Navarro Martinez <chupirrapucha@gmail.com>
2025-03-02 12:40:48 +0100

    74443290H creada la clase Game, hecho el constructor

---
+++ hada-p2/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    public class Game
+    {
+        private bool finPartida;
+        private Tablero tablero;
+        private List<Barco> barcos;
+
+        public Game()
+        {
+            finPartida = false;
+            barcos = new List<Barco>();
+            gameLoop();  // Llamamos al bucle del juego desde el constructor.
+        }
+        private void gameLoop()
+        {
+            barcos.Add(new Barco("Barco1", 3, 'h', new Coordenada(0, 0)));
+            barcos.Add(new Barco("Barco2", 4, 'v', new Coordenada(2, 3)));
+            barcos.Add(new Barco("Barco3", 2, 'h', new Coordenada(5, 5)));
+            tablero = new Tablero(10, barcos);
+            tablero.eventoFinPartida += cuandoEventoFinPartida;
+        }
+
+    }

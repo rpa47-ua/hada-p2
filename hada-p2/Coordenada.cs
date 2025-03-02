573d7a826aac9aeb9ad9060adea07824c7ba16a5
Francisco Navarro Martinez <chupirrapucha@gmail.com>
2025-03-02 12:10:17 +0100

    74443290H Hecha ambas clases de los argumentos de los eventos

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Hada
 {
-    class Coordenada
+    public class Coordenada
     {
         private int fila;
         private int columna;
---
+++ hada-p2/Eventos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    public class TocadoArgs : EventArgs
+    {
+        public string Nombre { get; }
+        public Coordenada CoordenadaImpacto { get; }
+
+        public TocadoArgs(string nombre, Coordenada coordenadaImpacto)
+        {
+            Nombre = nombre;
+            CoordenadaImpacto = coordenadaImpacto;
+        }
+    }
+    public class HundidoArgs : EventArgs
+    {
+        public string Nombre { get; }
+
+        public HundidoArgs(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+
+}

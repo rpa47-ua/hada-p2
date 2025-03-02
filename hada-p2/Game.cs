2d643f0d30eea0ddaa9b93e9c5e92607c476530f
Francisco Navarro Martinez <chupirrapucha@gmail.com>
2025-03-02 13:09:05 +0100

    74443290H Acabada la clase game

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -25,6 +25,63 @@ namespace Hada
             barcos.Add(new Barco("Barco3", 2, 'h', new Coordenada(5, 5)));
             tablero = new Tablero(10, barcos);
             tablero.eventoFinPartida += cuandoEventoFinPartida;
+            while (!finPartida)
+            {
+                Console.Clear();
+                Console.WriteLine(tablero.ToString());  // Mostramos el estado actual del tablero.
+
+
+                Console.Write("Introduce una coordenada (fila,columna) o 's' para salir: ");
+                string input = Console.ReadLine();
+
+
+                if (input.ToLower() == "s")
+                {
+                    finPartida = true;
+                    Console.WriteLine("La partida ha sido finalizada por el usuario.");
+                    break;
+                }
+
+
+                if (validarCoordenada(input, out Coordenada coordenada))
+                {
+
+                    tablero.Disparar(coordenada);
+                }
+                else
+                {
+                    Console.WriteLine("Coordenada no válida. Asegúrate de introducir el formato correcto.");
+                }
+            }
+        }
+        private bool validarCoordenada(string input, out Coordenada coordenada)
+        {
+            coordenada = null;
+
+
+            if (input.Length == 3)
+            {
+
+                if (input[1] == ',')
+                {
+
+                    if (int.TryParse(input[0].ToString(), out int fila) && int.TryParse(input[2].ToString(), out int columna))//mirar esto
+                    {
+                        coordenada = new Coordenada(fila, columna);
+                        return true;
+                    }
+                }
+            }
+
+            
+            return false;
+        }
+
+        private void cuandoEventoFinPartida(object sender, EventArgs e)
+        {
+            Console.WriteLine("¡¡PARTIDA FINALIZADA!!");
+            finPartida = true;
         }
 
     }
+}
